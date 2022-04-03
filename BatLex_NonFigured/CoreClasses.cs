406b5f3980080d6be9fde641a82ac7b982071459
GroophyLifefor <77299279+GroophyLifefor@users.noreply.github.com>
2022-04-03 16:28:15 +0300

    Add files via upload
    Modernized

---
+++ BatLex_NonFigured/CoreClasses.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace BatLex_NonFigured
+{
+    class CoreClasses
+    {
+        public struct errors
+        {
+            public int Line { get; set; }
+            public twot[] Errors { get; set; }
+        }
+
+        public struct twot
+        {
+            public int StartIndex { get; set; }
+            public int Lenght { get; set; }
+        }
+
+        public struct label
+        {
+            public int Line { get; set; }
+            public string Name { get; set; }
+        }
+
+        public enum Types
+        {
+            KeyWord,
+            Varriable,
+            Bracked,
+            UpdateKey,
+            Parameter
+        }
+
+        public struct LexStruct
+        {
+            public string Word { get; set; }
+            public string Desc { get; set; }
+            public Types Type { get; set; }
+        }
+    }
+}
